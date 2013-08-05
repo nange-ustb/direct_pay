62c20d5ce3fb9f5293f741ca9eda28fe0ad7ffe2
wanggc <sa@ubuntu.(none)>
2013-08-05 17:03:37 +0800

    cannot sort

--- lib/direct_pay/sign.rb
+++ lib/direct_pay/sign.rb
@@ -3,8 +3,8 @@ require 'digest/md5'
 module DirectPay
   module Sign
     def self.generate(params)
-      query = params.sort.map do |key, value|
-        "#{key}=#{value}"
+      query = params.map do |key, value|
+        "#{key.to_s}=#{value}"
       end.join('&')
 
       Digest::MD5.hexdigest("#{query}#{DirectPay.key}")
